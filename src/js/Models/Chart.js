90c1b30909850c9e337281e7c9f3134fee4dadea
CarlosCanizal <carlos@canizal.com>
2019-03-20 11:47:06 -0600

    Adds validation to Chart Model params and testit

--- src/js/Models/Chart.js
+++ src/js/Models/Chart.js
@@ -1,7 +1,17 @@
-const Chart = (title, total) =>{
+const Chart = (title, total, data, units='') =>{
+
+    if(!(typeof title === 'string'))throw "'title' should be a string";
+    if(!(typeof total === 'number'))throw "'total' should be a number";
+    if(!(typeof title === 'string'))throw "'title' should be a string";
+    if(!Array.isArray(data)) throw "'data' should be an array";
+    if(!data.length) throw "'data' should not be an empty array";
+    if(!(typeof units === 'string'))throw "'units' should be a string";
+
     let state = {
         title : title,
-        total : total
+        total : total,
+        data : data,
+        units : units
     }
 
     return Object.assign({},state);
--- tests/ChartModel.test.js
+++ tests/ChartModel.test.js
@@ -1,9 +1,41 @@
 import Chart from '../src/js/Models/Chart';
 
-test('the response when calls Chart Model ', () => {
-    const graph = Chart('Tilte1',200,[]);
+test('throws when \'title\' is not a String', () => {
+    expect(() => {
+        Chart(1,200,[1,2,3])
+    }).toThrow();
+});
+
+test('throws when \'total\' is not a Number', () => {
+    expect(() => {
+        Chart('Chart Title','200',[1,2,3])
+    }).toThrow();
+});
+
+test('throws when \'data\' is not passed as array', () => {
+    expect(() => {
+        Chart('Chart Title',200.34,1)
+    }).toThrow();
+});
+
+test('throws when \'data\' is empty an array', () => {
+    expect(() => {
+        Chart('Chart Title',200,[])
+    }).toThrow();
+});
+
+test('the response when calls Chart Model with initial params', () => {
+    const graph = Chart('Chart Title',200,[1,2,3]);
     expect(graph).toEqual(expect.objectContaining({
         title : expect.any(String),
-        total : expect.any(Number)
+        total : expect.any(Number),
+        data : expect.any(Array),
+        units : expect.any(String)
     }));
+});
+
+test('throws when \'units\' is not a string', () => {
+    expect(() => {
+        Chart('Chart Title',200.34,[1,2,3],1)
+    }).toThrow();
 });
