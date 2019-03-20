d2b957a60aa3d75000bf9d078f9ed51a030f0704
CarlosCanizal <carlos@canizal.com>
2019-03-20 12:13:26 -0600

    Adds validation to ChartData Model and test it

--- src/js/Models/ChartData.js
+++ src/js/Models/ChartData.js
@@ -1,4 +1,10 @@
 let ChartData = (label, percent, value)=>{
+    if(!(typeof label === 'string'))throw "'label' should be a string";
+    if(!(typeof percent === 'number'))throw "'percent' should be a number";
+    if(!(percent >= 0 && percent <=100))throw "'percent' should be in 0-100 range";
+    if(!(typeof value === 'number'))throw "'value' should be a number";
+    
+
     let state  = {
         label : label,
         percent : percent,
--- tests/ChartDataModel.test.js
+++ tests/ChartDataModel.test.js
@@ -1,10 +1,54 @@
 import ChartData from '../src/js/Models/ChartData';
 
+test('it throws when \'title\' is not a String', () => {
+    expect(() => {
+        ChartData(1,40,40);
+    }).toThrow();
+});
+
+test('it throws when \'percent\' is not a Number', () => {
+    expect(() => {
+        ChartData('Label 1','40',40);
+    }).toThrow();
+});
+
+test('it throws when \'percent\' is not in 0-100 range', () => {
+    expect(() => {
+        ChartData('Label 1',101,40);
+    }).toThrow();
+});
+
+test('it throws when \'value\' is not a Number', () => {
+    expect(() => {
+        ChartData('Label 1',40,'40');
+    }).toThrow();
+});
+
 test('the response when calls ChartData Model with initial params', () => {
     const data = ChartData('Label 1',40,40);
+    console.log(data);
     expect(data).toEqual(expect.objectContaining({
         label : expect.any(String),
-        percent : expect.any(Number),
+        percent : expect.toBeWithinRange(0, 100),
         value: expect.any(Number)
     }));
 });
+
+expect.extend({
+    toBeWithinRange(received, floor, ceiling) {
+      const pass = received >= floor && received <= ceiling;
+      if (pass) {
+        return {
+          message: () =>
+            `expected ${received} not to be within range ${floor} - ${ceiling}`,
+          pass: true,
+        };
+      } else {
+        return {
+          message: () =>
+            `expected ${received} to be within range ${floor} - ${ceiling}`,
+          pass: false,
+        };
+      }
+    },
+  });
