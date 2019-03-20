76cee283bfab176bf062a2a24ec905e27b23e0d3
CarlosCanizal <carlos@canizal.com>
2019-03-20 11:05:51 -0600

    Creates webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: ["./src/js/index.js","./src/css/main.sass"],
+  output: {
+    path: path.resolve(__dirname, "src/dist"),
+    publicPath: "/src/dist/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.(scss|sass)$/,
+        exclude: /(node_modules)/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [ 
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
